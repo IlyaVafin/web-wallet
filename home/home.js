b0d27fb9a54b34af5386411cd906a374acd568ff
IlyaVafin <rthrthrer5gh@gmail.com>
2026-03-05 22:04:49 +0300

    fix: #31 fix urls again

--- home/home.js
+++ home/home.js
@@ -322,7 +322,7 @@ logoutButton.forEach(btn => {
 	btn.addEventListener("click", () => {
 		document.cookie =
 			"isAuthorized=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
-		window.location.href = "/login/login.html"
+		window.location.href = "/web-wallet/login/login.html"
 	})
 })
 
